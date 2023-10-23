699ee7659465e0b0531ec2a158a5f2851f267f17
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-23 13:36:58 +0100

    more testing trying fix issues

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,27 +1,31 @@
 <template>
-    <v-row class="d-flex">
-      <v-col cols="2">
-        <LogoPielCanela />
-      </v-col>
-      <v-col>
-        <TheWelcome />
-      </v-col>
-      <v-col cols="2">
-        <IconosRedesSociales />
-      </v-col>
-    </v-row>
-  </template>
-  
-  <script setup>
-  import LogoPielCanela from '../components/LogoPielCanela.vue';
-  import TheWelcome from '../components/TheWelcome.vue';
-  import IconosRedesSociales from '../components/IconosRedesSociales.vue';
-  </script>
-  
-  <style>
-  .d-flex {
-    display: flex;
-    align-items: center; /* Alinea verticalmente en el centro */
-  }
-  </style>
-  
+  <v-container class="d-flex">
+    <v-col cols="2">
+      <LogoPielCanela />
+    </v-col>
+    <v-col cols="8">
+      <TheWelcome />
+    </v-col>
+    <v-col cols="2">
+      <IconosRedesSociales />
+    </v-col>
+  </v-container>
+</template>
+
+<script setup>
+import LogoPielCanela from '../components/LogoPielCanela.vue';
+import TheWelcome from '../components/TheWelcome.vue';
+import IconosRedesSociales from '../components/IconosRedesSociales.vue';
+</script>
+
+
+<style>
+.d-flex {
+  display: flex;
+  justify-content: space-between; /* Espacio uniforme entre los componentes */
+  align-items: center; /* Alinea verticalmente en el centro */
+}
+.text-center {
+  text-align: center; /* Alineación horizontal en el centro */
+}
+</style>
