8e78ec1a48a56c8f0aefaa6d938f4479dcb919dd
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-23 13:29:44 +0100

    Creado las "pruebas" de los botones tratando de arreglar un problema con MDI icons

--- src/components/IconosRedesSociales.vue
+++ src/components/IconosRedesSociales.vue
@@ -1,27 +1,32 @@
 <template>
- <v-row>
-     <v-col>
+    <v-row>
+      <v-col class="icon-col">
         <v-btn>
-            <v-icon>mdi-instagram</v-icon>
+          <a href="https://www.instagram.com" target="_blank"><v-icon> Prueba 1</v-icon></a>
         </v-btn>
-     </v-col>
-     <v-col>
+        <br> 
+      </v-col>
+      <v-col class="icon-col">
         <v-btn>
-
+          <a href="https://www.linkedin.com" target="_blank"><v-icon> Prueba 2</v-icon></a>
         </v-btn>
-    </v-col>
-    <v-col>
+        <br>
+      </v-col>
+      <v-col class="icon-col">
         <v-btn>
-            
+          <a href="https://www.facebook.com" target="_blank"><v-icon> Prueba 3</v-icon></a>
         </v-btn>
-    </v-col>
- </v-row>
-</template>
-
-<script >
-
-</script>
-
-<style scoped>
-
-</style>
+      </v-col>
+    </v-row>
+  </template>
+  
+  <script>
+  </script>
+  
+  <style scoped>
+  .icon-col {
+    margin-bottom: 20px;
+    /* Ajusta el valor según tu preferencia */
+  }
+  </style>
+  
