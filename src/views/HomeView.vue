1e5b027528ba945b2f5b1135b6489ab635c25af9
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-23 12:36:22 +0100

    Homeview RedesSocialesIcons imported

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,16 @@
 <template>
-    <LogoPielCanela />
-    <TheWelcome />
+    <v-row>
+        <v-col>
+            <LogoPielCanela />
+            <TheWelcome />
+            <IconosRedesSociales />
+        </v-col>
+    </v-row>
 </template>
 <script setup>
-import LogoPielCanela from  '../components/LogoPielCanela.vue';
+import LogoPielCanela from '../components/LogoPielCanela.vue';
 import TheWelcome from '../components/TheWelcome.vue'
+import IconosRedesSociales from '../components/IconosRedesSociales.vue';
 </script>
 
-<style>
-</style>
+<style></style>
