764ec77231803387d5481f5fd79c6b7d783ffeae
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-27 07:16:28 +0100

    Se añadio la grafica del gif

--- src/components/Identidad.vue
+++ src/components/Identidad.vue
@@ -1,22 +1,24 @@
 <template>
     <div class="center-content">
-        Identidad Gráfica
+      <img class="palabra" src="@/assets/palabras.gif" alt="Palabras">
     </div>
-</template>
-
-<style scoped>
-.center-content {
+  </template>
+  
+  <style scoped>
+  .palabra{
+    height: auto;
+    width: 50%;
+  }
+  .center-content {
     display: flex;
-    justify-content: center;
-    /* Centra horizontalmente */
-    align-items: center;
-    /* Centra verticalmente */
+    justify-content: center; /* Centra horizontalmente */
+    align-items: center; /* Centra verticalmente */
     font-family: 'Playfair Display', serif;
     font-size: 10vh;
     line-height: 1em;
     color: rgb(255, 255, 255);
-    height: 100vh;
-    /* Esto asegura que ocupe toda la altura de la pantalla */
-}
-</style>
+    height: 100vh; /* Esto asegura que ocupe toda la altura de la pantalla */
+  }
+  </style>
+  
   
