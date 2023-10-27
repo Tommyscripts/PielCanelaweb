6970fd178a624ed1787d6b2286b572647945389b
Tommyscripts <javier.cabrera.escoz@gmail.com>
2023-10-27 08:25:41 +0100

    arreglando vista "homeview" para adaptarla a movil

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -4,9 +4,14 @@
       <LogoPielCanela />
     </v-col>
     <v-col cols="8">
-      <TheWelcome />
+      <div class="caja1">
+        <TheWelcome />
+        <div class="mobile-icons">
+          <IconosRedesSociales />
+        </div>
+      </div>
     </v-col>
-    <v-col cols="2">
+    <v-col cols="2" class="desktop-icons">
       <IconosRedesSociales />
     </v-col>
   </div>
@@ -18,22 +23,44 @@ import TheWelcome from '../components/TheWelcome.vue';
 import IconosRedesSociales from '../components/IconosRedesSociales.vue';
 </script>
 
-
 <style scoped>
 .tamano {
   height: 100%;
   width: 100%;
   background-color: black;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
 }
 
 .d-flex {
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-
 }
 
-.text-center {
-  text-align: center;
+.mobile-icons {
+  display: none;
+}
+
+.desktop-icons {
+  display: block;
+}
+
+.caja1 {
+  position: relative;
+}
+
+/* Media query para pantallas con un ancho máximo de 900px (ajustado para dispositivos similares al iPhone SE) */
+@media (max-width: 900px) {
+  .mobile-icons {
+    display: flex;
+    flex-direction: row; /* Mostramos los iconos horizontalmente en la vista móvil */
+    justify-content: center; /* Centramos los iconos horizontalmente en la vista móvil */
+  }
+  .desktop-icons {
+    display: none;
+  }
+  .caja1 {
+    position: relative; /* Aseguramos que 'caja1' siga siendo 'relative' */
+  }
 }
 </style>
